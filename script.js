67ed4d8b000ecb203236796bd489155d9bdc79d7
Chriss1303 <macahiliglaurence1@gmail.com>
2024-12-15 23:32:47 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("home").addEventListener("click", function() {
+    alert("Welcome to the Home Section!");
+});
+
+document.getElementById("about").addEventListener("click", function() {
+    alert("Welcome to the About Section!");
+});
